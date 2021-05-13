07d93c6a76026d33a4ae499bed643319a8e836a4
Sofiag8 <sofiag8@holbertonschool.com>
2021-05-12 21:47:21 -0500

    fixing pep

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,11 +10,12 @@ def minOperations(n):
     checking minimun operations
     """
 
+    if min_number_operations <= 1:
+        return 0
+
     min_number_operations = 0
     available_operations = 2
 
-    if min_number_operations <= 1:
-        return 0
     while n > 1:
         if n % available_operations == 0:
             n = n / available_operations
